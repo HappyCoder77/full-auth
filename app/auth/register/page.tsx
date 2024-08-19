4d13e2a8c0847eba181e8e9aecf2607b1a990ed1
HappyCoder77 <saul_rr77@hotmail.com>
2024-08-19 12:21:08 -0400

    edit metadata and text of register page

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -4,8 +4,8 @@ import { Metadata } from "next";
 import { SocialButtons } from "@/components/common";
 
 export const metadata: Metadata = {
-  title: "Full Auth | Register",
-  description: "Full Auth Register Page",
+  title: "Mis Barajitas | Registro",
+  description: "Página de registro de Mis Barajitas",
 };
 export default function Page() {
   // return method
@@ -15,27 +15,27 @@ export default function Page() {
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          alt="Full Auth"
+          alt="Mis Barajitas"
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
           className="mx-auto h-10 w-auto"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign up to your account
+          Crea una cuenta
         </h2>
       </div>
       {/* form container */}
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         {/* form */}
         <RegisterForm />
-        <SocialButtons />
+        {/* <SocialButtons /> */}
         {/* login link */}
         <p className="mt-10 text-center text-sm text-gray-500">
-          Already have an account?{" "}
+          ¿Ya tienes una cuenta?{" "}
           <Link
             href="/auth/login/"
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
           >
-            Login here
+            Inicia sesión aquí
           </Link>
         </p>
       </div>
